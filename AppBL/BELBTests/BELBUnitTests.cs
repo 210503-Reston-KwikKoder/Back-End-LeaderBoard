734445ae9600a15cffcce24eb68b33e6ed88d556
Robert Fisher <RobertNFisherJR@gmail.com>
2021-06-24 09:58:45 -0400

    Finished test for Delete and adding average tables

--- AppBL/BELBTests/BELBUnitTests.cs
+++ AppBL/BELBTests/BELBUnitTests.cs
@@ -6,6 +6,8 @@ using BELBDL;
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
 using Serilog;
+using System.Collections.Generic;
+
 namespace BELBTests
 {
     public class BELBUnitTests
@@ -75,44 +77,6 @@ namespace BELBTests
         /// Leaderboard tests
         /// </summary>
 
-        /*[Fact]
-        public async Task AddLeaderboardShouldAddLeaderboardAsync()
-        {
-            using (var context = new BELBDBContext(options))
-            {
-                ILeaderboardBL leaderboardBL = new LeaderboardBL(context);
-                LeaderBoard leaderboard = new LeaderBoard();
-                leaderboard.AuthId = "CM";
-                leaderboard.UserName = "Cesar123";
-                leaderboard.Name = "Cesar";
-                leaderboard.AverageWPM = 25;
-                leaderboard.AverageAcc = 5;
-                leaderboard.CatID = 1;
-                //category.Name = 1;
-                await leaderboardBL.AddLeaderboard(leaderboard);
-                LeaderBoard leaderboard1 = new LeaderBoard();
-                leaderboard1.AuthId = "CM";
-                leaderboard1.UserName = "Cesar123";
-                leaderboard1.Name = "Cesar";
-                leaderboard1.AverageWPM = 65;
-                leaderboard1.AverageAcc = 50;
-                leaderboard1.CatID = 3;
-                //category1.Name = 2;
-                await leaderboardBL.AddLeaderboard(leaderboard1);
-                LeaderBoard leaderboard2 = new LeaderBoard();
-                leaderboard2.AuthId = "RF";
-                leaderboard2.UserName = "Robbie789";
-                leaderboard2.Name = "Robbie";
-                leaderboard2.AverageWPM = 85;
-                leaderboard2.AverageAcc = 96;
-                leaderboard2.CatID = 1;
-                //category2.Name = 3;
-                await leaderboardBL.AddLeaderboard(leaderboard2);
-                int catCount = (await leaderboardBL.GetAllLeaderboards()).Count;
-                int expected = 5;
-                Assert.Equal(expected, catCount);
-            }
-        }*/
 
         [Fact]
         public async Task AddingLeaderboardTwiceShouldBeNull()
@@ -129,6 +93,54 @@ namespace BELBTests
                 leaderboard.CatID = 1;
                 leaderboardBL.AddLeaderboard(leaderboard);
                 Assert.Null(await leaderboardBL.AddLeaderboard(leaderboard));
+            }
+        }
+
+        [Fact]
+        public async Task AddingLeaderBoardShouldCreateOrUpdateAnAverageLeaderBoard()
+        {
+            using (var context = new BELBDBContext(options))
+            {
+                ILeaderboardBL leaderboardBL = new LeaderboardBL(context);
+                LeaderBoard leaderboard = new LeaderBoard()
+                {
+                    AuthId = "CM",
+                    UserName = "Cesar123",
+                    Name = "Cesar",
+                    AverageWPM = 25,
+                    AverageAcc = 5,
+                    CatID = 1
+                };
+                await leaderboardBL.AddLeaderboard(leaderboard);
+                List<LeaderBoard> Result = await leaderboardBL.GetLeaderboardByCatId(-2);
+
+                // This should create an average leaderboard under CatID -2
+                Assert.NotNull(Result);
+            }
+        }
+
+        [Fact]
+        public async Task deleteLeaderBoardShouldRemoveLeaderBoard()
+        {
+            using (var context = new BELBDBContext(options))
+            {
+                ILeaderboardBL leaderboardBL = new LeaderboardBL(context);
+                LeaderBoard leaderboard = new LeaderBoard()
+                {
+                    AuthId = "CM",
+                    UserName = "Cesar123",
+                    Name = "Cesar",
+                    AverageWPM = 25,
+                    AverageAcc = 5,
+                    CatID = 1
+                };
+                leaderboardBL.AddLeaderboard(leaderboard);
+                leaderboardBL.DeleteLeaderboard(leaderboard.AuthId, leaderboard.CatID);
+                List<LeaderBoard> Result = await leaderboardBL.GetLeaderboardByCatId(1);
+
+                int expected = 1;
+                // This should create an average leaderboard under CatID -2
+                Assert.Equal(Result.Count, expected);
             }
         }
 
