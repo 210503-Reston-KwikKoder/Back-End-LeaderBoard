0c3898dd31befe0ddaed0d1d559280c35074e3a6
Cesar <cesartamu2019@gmail.com>
2021-06-25 16:27:08 -0500

    Code Smell Changes

--- AppBL/BELBRest/Controllers/LBController.cs
+++ AppBL/BELBRest/Controllers/LBController.cs
@@ -25,20 +25,11 @@ namespace BELBRest.Controllers
             _leaderboardBL = leaderboardBL;
             _ApiSettings = settings.Value;
         }
-        /// <summary>
-        /// GET /api/LB
-        /// General leaderboard, gets the best users in general to send to client
-        /// </summary>
-        /// <returns>List of best users in the database sorted by WPM or 404 if it cannot be found</returns>
+        
         [HttpGet]
         public async Task<IActionResult> GetAllLeaderboards()
         {
-            /*Get 
-            Get List users from data base
-            foreach UserName AVG(AverageWPM / AverageAcc) < --Could Cause high runtimes, solution? storing as independent Table
-
-            Rank(results)
-            */
+            
             return Ok(await _leaderboardBL.GetAllLeaderboards());
 
         }
@@ -46,9 +37,6 @@ namespace BELBRest.Controllers
         [HttpGet("{id}")]
         public async Task<IActionResult> GetLeaderboardByCatID(int id)
         {
-            /*Get { CatID}
-            -Get data for users whose rank < 101 && this.CatID == CatID
-            */
             return Ok(await _leaderboardBL.GetLeaderboardByCatId(id)); // Just have this to prevent errors for now...
         }
 
@@ -73,19 +61,6 @@ namespace BELBRest.Controllers
                 return BadRequest();
             }
         }
-        /// <summary>
-        /// Private method to get application token for auth0 management 
-        /// </summary>
-        /// <returns>dynamic object with token for Auth0 call</returns>
-        private dynamic GetApplicationToken()
-        {
-            var client = new RestClient("https://kwikkoder.us.auth0.com/oauth/token");
-            var request = new RestRequest(Method.POST);
-            request.AddHeader("content-type", "application/json");
-            request.AddParameter("application/json", _ApiSettings.authString, ParameterType.RequestBody);
-            IRestResponse response = client.Execute(request);
-            Log.Information("Response: {0}",response.Content);
-            return JsonConvert.DeserializeObject(response.Content);
-        }
+        
     }
 }
