d1e94aa5544c83977e03944d3a46d5fba9f8da45
Robert Fisher <RobertNFisherJR@gmail.com>
2021-06-25 11:06:38 -0400

    added migrations to new DB

---
+++ AppBL/BELBDL/Migrations/20210625150623_initial.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BELBDL.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    CId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LeaderBoards",
+                columns: table => new
+                {
+                    AuthId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    CatID = table.Column<int>(type: "int", nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AverageWPM = table.Column<double>(type: "float", nullable: false),
+                    AverageAcc = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LeaderBoards", x => new { x.AuthId, x.CatID });
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "LeaderBoards");
+        }
+    }
+}
